6efb6498a090f542fab5f3124305622072130d6a
shreeshail-chavan <shreeshailchavan309@gmail.com>
2025-03-17 23:40:28 +0530

    all divisors

---
+++ striver_a2z/basic_maths/alldivisors.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printDivisors(int &n){
+    set<int> nums;
+    for(int i=1;i<=sqrt(n);i++){
+        if(n%i == 0){
+            nums.emplace(i);
+            nums.emplace(n/i);
+        }
+    }
+    for(auto it=nums.begin();it!=nums.end();it++)
+        cout<<*(it)<<endl;
+}
+
+int main(){
+    int n = 5;
+    printDivisors(n);
+    return 0;
+}
